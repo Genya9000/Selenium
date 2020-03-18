9431289bacbd76b641a149644bc3456290de7661
Evgeniy Haliman <eugen.haliman@gmail.com>
2020-03-18 21:16:10 +0200

    add 2 tests

--- src/test/java/page/CloudGoogleHomePage.java
+++ src/test/java/page/CloudGoogleHomePage.java
@@ -1,5 +1,7 @@
 package page;
 
+import com.sun.xml.internal.bind.v2.model.core.ID;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,22 +17,60 @@ public class CloudGoogleHomePage {
     private WebDriver driver;
     private WebDriverWait wait;
     private static final String HOMEPAGE_URL = "https://cloud.google.com/ ";
-    @FindBy(xpath = "//div[@class='devsite-search-image material-icons']")
+    @FindBy(xpath = "//input[@class='devsite-search-field devsite-search-query']")
     private WebElement searchBotton;
     @FindBy(linkText = "Google Cloud Platform Pricing Calculator")
     private WebElement calculatorLink;
-    @FindBy(xpath = "//div[@class='name ng-binding'][text()='Compute Engine']")
-    private List<WebElement> computeEngineSection;
-    @FindBy(xpath = "//input[@id='input_55']")
+    @FindBy(xpath = "//*[@id=\"mainForm\"]/md-tabs/md-tabs-wrapper/md-tabs-canvas/md-pagination-wrapper/md-tab-item[1]/div[1]/div")
+    private WebElement computeEngineSection;
+    @FindBy(id = "input_55")
     private WebElement numberInstancesInput;
     @FindBy(xpath = "//md-select-value[@id='select_value_label_52']")
     private WebElement instanceTypeSelect;
+    @FindBy(xpath = "//div[contains(text(), '       n1-standard-8 (vCPUs: 8, RAM: 30GB)')]")
+    private WebElement n1Standart8;
+    @FindBy(xpath = "//div[contains(text(), '  Add GPUs.')]")
+    private WebElement addGpuCheckBox;
+    @FindBy(xpath = "//md-select[@id=\"select_897\"]")
+    private WebElement numberOfGpusInput;
+    @FindBy(xpath = "//md-option[@id='select_option_331']")
+    private WebElement count1Gpu;
+    @FindBy(id = "select_328")
+    private WebElement gpyType;
+    @FindBy(id = "select_option_338")
+    private WebElement typeTeslaV100;
+    @FindBy(id = "select_167")
+    private WebElement localSsdField;
+    @FindBy(id = "select_option_229")
+    private WebElement ssdCapacity;
+    @FindBy(id = "select_82")
+    private WebElement dataLocationInput;
+    @FindBy(id = "select_option_177")
+    private WebElement locationfrankfurt;
+    @FindBy(id = "select_89")
+    private WebElement commitedUsageInput;
+    @FindBy(id = "select_option_102")
+    private WebElement usageYear;
+    @FindBy(xpath = "//button[contains(text(),  \"    Add to Estimate\")][@class='md-raised md-primary cpc-button md-button md-ink-ripple'][@ng-click='listingCtrl.addComputeServer(ComputeEngineForm);']")
+    private WebElement addToEstimateButton;
+    @FindBy(xpath = "//div[@class='md-list-item-text ng-binding'][contains(text(), 'VM')]")
+    private WebElement vMClassField;
+    @FindBy(xpath = "//div[@class='md-list-item-text ng-binding'][contains(text(), 'Instance')]")
+    private WebElement instanceTypeField;
+    @FindBy(xpath = "//div[@class='md-list-item-text ng-binding'][contains(text(), 'Region')]")
+    private WebElement regionField;
+    @FindBy(xpath = "//div[@class='md-list-item-text ng-binding'][contains(text(), 'local')]")
+    private WebElement localSSDValue;
+    @FindBy(xpath = "//b[@class=\"ng-binding\"][contains(text(), 'Cost:')]")
+    private List<WebElement> paymentValues;
+
+
 
 
 
     public CloudGoogleHomePage(WebDriver driver) {
         this.driver = driver;
-        wait = new WebDriverWait(driver, 15, 50);
+        wait = new WebDriverWait(driver, 10, 50);
         PageFactory.initElements(driver, this);
     }
     public CloudGoogleHomePage openPage(){
@@ -47,13 +87,48 @@ public class CloudGoogleHomePage {
         return this;
     }
     public CloudGoogleHomePage selectComputeEngineSection(){
-        wait.until(ExpectedConditions.visibilityOf(computeEngineSection.get(0))).click();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.switchTo().frame(0).switchTo().frame("myFrame");
+        wait.until(ExpectedConditions.visibilityOf(computeEngineSection)).click();
         return this;
     }
     public CloudGoogleHomePage fillInTheForm(){
+        numberInstancesInput.click();
         numberInstancesInput.sendKeys("4");
-
+        instanceTypeSelect.click();
+        wait.until(ExpectedConditions.visibilityOf(n1Standart8)).click();
+        addGpuCheckBox.click();
+        wait.until(ExpectedConditions.visibilityOf(numberOfGpusInput)).click();
+        wait.until(ExpectedConditions.visibilityOf(count1Gpu)).click();
+        gpyType.click();
+        wait.until(ExpectedConditions.visibilityOf(typeTeslaV100)).click();
+        localSsdField.click();
+        wait.until(ExpectedConditions.visibilityOf(ssdCapacity)).click();
+        dataLocationInput.click();
+        wait.until(ExpectedConditions.visibilityOf(locationfrankfurt)).click();
+        commitedUsageInput.click();
+        wait.until(ExpectedConditions.visibilityOf(usageYear)).click();
+        addToEstimateButton.click();
+        return this;
     }
-
+public boolean vmClassContainRegular(){
+        return wait.until(ExpectedConditions.visibilityOf(vMClassField)).getText().contains("regular");
+}
+public boolean instanceTypeCheck(){
+        return instanceTypeField.getText().contains("n1-standard-8");
+}
+public boolean regionCheck(){
+        return regionField.getText().contains("Frankfurt");
+}
+public boolean localSSDCheck(){
+        return localSSDValue.getText().contains("2x375 GB");
+}
+public boolean paymentCheck(){
+        return paymentValues.get(0).getText().contains("1,288.71");
+}
 
 }
--- src/test/java/testFromTask/HurtMePlentyTest.java
+++ src/test/java/testFromTask/HurtMePlentyTest.java
@@ -1,23 +1,46 @@
 package testFromTask;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import page.CloudGoogleHomePage;
 import page.PastebinHomePage;
+import java.util.concurrent.TimeUnit;
 
 public class HurtMePlentyTest {
     private WebDriver driver;
     private CloudGoogleHomePage cloudGoogleHomePage;
-    private String CODE_TEXT = "git config --global user.name  \\\"New Sheriff in Town\\\"\\n\" +\n" +
-            "                        \"git reset $(git commit-tree HEAD^{tree} -m \\\"Legacy code\\\")\\n\" +\n" +
-            "                        \"git push origin master --force";
+    private String SEARCH_TEXT = "Google Cloud Platform Pricing Calculator";
     private String TITLE_TEXT = "how to gain dominance among developers";
     @Test
+    public void fieldCheck(){
+        cloudGoogleHomePage.openPage()
+                .enterInSearchBox(SEARCH_TEXT)
+                .clickOnCalcolatorLink()
+                .selectComputeEngineSection()
+                .fillInTheForm();
+        Assert.assertEquals(true, cloudGoogleHomePage.vmClassContainRegular());
+        Assert.assertEquals(true, cloudGoogleHomePage.instanceTypeCheck());
+        Assert.assertEquals(true, cloudGoogleHomePage.localSSDCheck());
+        Assert.assertEquals(true, cloudGoogleHomePage.regionCheck());
+        Assert.assertEquals(true, cloudGoogleHomePage.paymentCheck());
+    }
 
+    @Test
+    public void fieldCheck1(){
+        driver.get("https://cloud.google.com/products/calculator");
+       /* (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("myFrame"));*/
+        driver.switchTo().frame(0).switchTo().frame("myFrame");
+
+
+    }
 
 
     @BeforeMethod(alwaysRun = true)
